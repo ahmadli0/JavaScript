c3b7bca97f144abecedcc47f217d74a99e8ff257
Said Ahmadli <68443078+saidahmadli@users.noreply.github.com>
2021-12-11 20:38:41 +0400

    Add files via upload

---
+++ Basicfunctions(JavaScript)/JS.TurnOnOffLight/function.js
@@ -0,0 +1,8 @@
+function poweroff(){
+    document.querySelector("#picture").src="assets/images/light_off.jpg";
+    document.querySelector("body").style.background="black";
+}
+function poweron(){
+    document.querySelector("#picture").src="assets/images/light_on.jpeg";
+    document.querySelector("body").style.background="#292a2f";
+}
